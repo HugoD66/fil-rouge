8758177c09757a637296d6c108b223dbb34d1f0f
HugoD66 <dessauw.hugo@gmail.com>
2025-10-18 19:54:32 +0200

    Add test in monorepo script, package modifications for aliases

---
+++ api/jest.config.cjs
@@ -0,0 +1,17 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    rootDir: '.',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/test/**/*.spec.ts'],
+    transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+    collectCoverageFrom: ['<rootDir>/**/*.(t|j)s'],
+    coverageDirectory: '<rootDir>/../coverage',
+    // mappe les alias TS (@fil-rouge/...) pour Jest
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>/',
+    }),
+};
--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,23 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import type { Server } from 'http';
 import { AppModule } from '@fil-rouge/api/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<Server>;
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication<any>() as Server;
+    app = moduleFixture.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', async () => {
-    await request(app.getHttpServer())
+    await request(app.getHttpServer() as import('http').Server)
       .get('/')
       .expect(200)
       .expect('Hello World!');
